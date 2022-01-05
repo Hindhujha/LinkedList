d07ec67cc7ba17ae1f41768feacc13fa21e74917
unknown <hindhujhasridhar@gmail.com>
2022-01-05 19:51:38 +0530

    CREATING A SIMPLE LIST

---
+++ LinkedList/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+   internal class Node
+    {
+        public int data;//values in node
+        public Node next;//address of the next node
+        public Node (int data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//creating a linked list
+            LinkedList list=new LinkedList();
+            //adding values in linked list
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
